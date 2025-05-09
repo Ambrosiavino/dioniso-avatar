df032a36025e96fdf963d3c087260ab3b522d678
Ambrosiavino <enrico.patrizio@ambrosiavino.com>
2025-05-09 16:24:21 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
+// pages/index.jsx
 import dynamic from 'next/dynamic';
 
 const Avatar3D = dynamic(() => import('../components/Avatar3D'), { ssr: false });
 
 export default function Home() {
   return (
-    <main className="bg-black">
+    <main style={{ backgroundColor: '#111', height: '100vh' }}>
       <Avatar3D />
     </main>
   );
